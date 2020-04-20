c3b530fe3c4108c5baea12fbf54fe8ae3b55a4ee
Night Lore <nathanlui2010@gmail.com>
2020-04-19 22:39:00 -0700

    Implementing d3js graph simulation

--- client/src/BarGraph.js
+++ client/src/BarGraph.js
@@ -10,11 +10,13 @@ class BarGraph extends Component {
       const canvasHeight = 400;
       const canvasWidth = 600;
       const scale = 20;
+      const offset = 90;
       const svgCanvas = d3.select(this.refs.canvas)
          .append("svg")
          .attr("width", canvasWidth)
          .attr("height", canvasHeight)
          .style("border", "1px solid black");
+
       svgCanvas.selectAll("rect")
          .data(data).enter()
             .append("rect")
@@ -23,11 +25,35 @@ class BarGraph extends Component {
             .attr("fill", "orange")
             .attr("x", (datapoint, iteration) => iteration * 45)
             .attr("y", (datapoint) => canvasHeight - datapoint * scale);
+
       svgCanvas.selectAll("text")
          .data(data).enter()
             .append("text")
             .attr("x", (dataPoint, i) => i * 45 + 10)
             .attr("y", (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
+            .text(dataPoint => dataPoint)
+
+
+
+
+      svgCanvas.selectAll("circle")
+         .data(data).enter()
+            .append("circle")
+            .attr("cx", (d, i) => (i+1) * offset)
+            .attr("cy", d => d * scale)
+            .attr("r", d => d * scale)
+            .attr("stroke", "green")
+            .attr("stroke-width", 1)
+            .attr("fill", "navy")
+
+      svgCanvas.selectAll("text")
+         .data(data).enter()
+            .append("text")
+            .attr("x", (dataPoint, i) => (i+1) * offset)
+            .attr("y", (dataPoint, i) => dataPoint * scale)
+            .attr("dominant-baseline", "middle")
+            .attr("text-anchor", "middle")
+            .attr("fill", "white")
             .text(dataPoint => dataPoint)
    }
    render() { return <div ref="canvas"></div> }
--- client/src/Graph.js
+++ client/src/Graph.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import * as d3 from 'd3'
 //import { forceLink } from "d3-force";
 
-class Graph extends Component {
+class NodeGraph extends Component {
 
    componentDidMount() {
       const data = [ 2, 4, 2, 6, 8 ];
@@ -12,59 +12,61 @@ class Graph extends Component {
    drawGraph(data)  {
       const canvasHeight = 400;
       const canvasWidth = 600;
-      const scale = 10;
-      const offset = 90;
+
       const svgCanvas = d3.select(this._rootNode)
          .append("svg")
          .attr("width", canvasWidth)
          .attr("height", canvasHeight)
          .style("border", "1px solid black");
 
-      /*
-      const nodes = [{x: 15, y: 25}];
-      const links = [];
+      const nodes = [
+         {id: 1},
+         {id: 2},
+         {id: 3},
+         {id: 4},
+         {id: 5},
+         {id: 6},
+      ];
+      const links = [
+         {source: 1, target: 2, value: 1},
+         {source: 2, target: 3, value: 1},
+         {source: 3, target: 4, value: 1},
+         {source: 4, target: 5, value: 1},
+         {source: 5, target: 6, value: 1},
+      ];
+
+      const simulation = d3.forceSimulation(nodes)
+         .force("link", d3.forceLink(links).id(d => d.id))
+         .force("charge", d3.forceManyBody())
+         .force("center", d3.forceCenter(canvasWidth / 2, canvasHeight / 2));
+
       const link = svgCanvas.append("g")
-         .attr("stroke", "#999")
-         .selectAll("line");
+            .attr("stroke", "#999")
+         .selectAll("line")
+         .data(links)
+         .join("line")
+            .attr("stroke-width", d => Math.sqrt(d.value));
 
       const node = svgCanvas.append("g")
-         .selectAll("circle");
-      
-      const simulation = d3.forceSimulation(nodes)
-         .force("charge", d3.forceManyBody().strength(-60))
-         .force("link", d3.forceLink(links))
-         .force("x", d3.forceX())
-         .force("y", d3.forceY())
-         .on("tick", () => {
-            node.attr("cx", d => d.x)
-               .attr("cy", d => d.y)
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+         .selectAll("circle")
+         .data(nodes)
+         .join("circle")
+            .attr("r", 5)
+            .attr("fill", "red")
 
-            link.attr("x1", d => d.source.x)
-               .attr("y1", d => d.source.y)
-               .attr("x2", d => d.target.x)
-               .attr("y2", d => d.target.y);
-         });
-      */
+      simulation.on("tick", () => {
+         link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y)
+         node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+      });
 
-      svgCanvas.selectAll("circle")
-         .data(data).enter()
-            .append("circle")
-            .attr("cx", (d, i) => (i+1) * offset)
-            .attr("cy", d => d * scale)
-            .attr("r", d => d * scale)
-            .attr("stroke", "green")
-            .attr("stroke-width", 1)
-            .attr("fill", "navy")
-
-      svgCanvas.selectAll("text")
-         .data(data).enter()
-            .append("text")
-            .attr("x", (dataPoint, i) => (i+1) * offset)
-            .attr("y", (dataPoint, i) => dataPoint * scale)
-            .attr("dominant-baseline", "middle")
-            .attr("text-anchor", "middle")
-            .attr("fill", "white")
-            .text(dataPoint => dataPoint)
    }
 
    shouldComponentUpdate() {
@@ -82,8 +84,8 @@ class Graph extends Component {
 function _createForceSimulation() {
    return d3.forceSimulation()
       .force("charge", d3.forceManyBody().strength(-60))
-      .force("x", d3.forceX())
-      .force("y", d3.forceY())
+      //.force("x", d3.forceX())
+      //.force("y", d3.forceY())
 }
 
-export default Graph
+export default NodeGraph
